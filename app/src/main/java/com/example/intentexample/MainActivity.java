b67142e6e4b4141622530c756e08926aa543a8e6
whdmsans <joeunmun@naver.com>
2022-11-07 22:27:47 +0900

    UI수정

--- app/src/main/java/com/example/intentexample/MainActivity.java
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.intentexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-//  버튼 선언 수정, 캘린더,   친구,    홈,   검색,  설정
-    Button btn_1, btn_2, btn_3, btn_4, btn_5;
+    //화면 전환 변수
+    private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Calendar calendar;
+    private Search search;
+    private Home home;
+    private Friends friends;
+
+    //뒤로가기 버튼 변수
     private long backBtnTime = 0;
 
     @Override
@@ -21,62 +34,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //버튼들 아이디 매칭
-        btn_1 = findViewById(R.id.btn_1);
-        btn_2 = findViewById(R.id.btn_2);
-        btn_3 = findViewById(R.id.btn_3);
-        btn_4 = findViewById(R.id.btn_4);
-        btn_5 = findViewById(R.id.btn_5);
-        //캘린더 버튼
-        btn_1.setOnClickListener(new View.OnClickListener() {
+        // 버튼에 따른 화면 설정
+        bottomNavigationView = findViewById(R.id.bottom_navi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Calendar Calendar = new Calendar();
-                transaction.replace(R.id.frame, Calendar);
-                transaction.commit();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_calendar:
+                        setFrag(0);
+                        break;
+                    case R.id.action_search:
+                        setFrag(1);
+                        break;
+                    case R.id.action_map:
+                        setFrag(2);
+                        break;
+                    case R.id.action_friends:
+                        setFrag(3);
+                        break;
+                }
+                return true;
             }
         });
-        //친구 버튼
-        btn_2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Friends Friends = new Friends();
-                transaction.replace(R.id.frame, Friends);
-                transaction.commit();
-            }
-        });
-        //홈 버튼
-        btn_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Home Home = new Home();
-                transaction.replace(R.id.frame, Home);
-                transaction.commit();
-            }
-        });
-        //검색 버튼
-        btn_4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Search Search = new Search();
-                transaction.replace(R.id.frame, Search);
-                transaction.commit();
-            }
-        });
-        //설정 버튼
-        btn_5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Option Option = new Option();
-                transaction.replace(R.id.frame, Option);
-                transaction.commit();
-            }
-        });
+
+        // 각 화면 연결
+        calendar = new Calendar();
+        search = new Search();
+        home = new Home();
+        friends = new Friends();
+        setFrag(2); // 첫 화면을 선택
+
     }
+
+    // 화면 교체 실행문
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, calendar);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, search);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, home);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, friends);
+                ft.commit();
+                break;
+        }
+    }
+
+
 //뒤로가기 버튼 2번 눌러서 종료
     @Override
     public void onBackPressed() {
