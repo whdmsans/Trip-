d222f49bb2e3e6f9f671431287653ed6414becaa
whdmsans <joeunmun@naver.com>
2022-11-18 02:23:40 +0900

    로그인 구현2-1

--- app/src/main/java/com/example/intentexample/LoginA.java
+++ app/src/main/java/com/example/intentexample/LoginA.java
@@ -3,12 +3,20 @@ package com.example.intentexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginA extends AppCompatActivity {
+
+    private EditText et_id, et_pass;
+    private Button btn_login, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_passa);
     }
 }
--- app/src/main/java/com/example/intentexample/Register.java
+++ app/src/main/java/com/example/intentexample/Register.java
@@ -2,13 +2,71 @@ package com.example.intentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
+
+    private EditText et_id, et_pass, et_name, et_email;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // 아이디 값 찾아주기
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_passa);
+        et_name = findViewById(R.id.et_name);
+        et_email = findViewById(R.id.et_email);
+
+        // 회원가입 버튼 클릭 시 수행
+        btn_register = findViewById(R.id.btn_registera);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // EditText에 현재 입력되어 있는 값을 가져온다.
+                String userID = et_id.getText().toString();
+                String userpass = et_pass.getText().toString();
+                String username = et_name.getText().toString();
+                String useremail = et_email.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success){ // 회원가입 성공
+                                Toast.makeText(getApplicationContext(), "회원가입 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Register.this, Login.class);
+                                startActivity(intent);
+                            }else{ // 회원가입 실패
+                                Toast.makeText(getApplicationContext(), "회원가입 실패", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                // 서버로 Volley를 이용해 요청
+                RegisterRequest registerRequest = new RegisterRequest(userID, userpass, username, useremail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Register.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
