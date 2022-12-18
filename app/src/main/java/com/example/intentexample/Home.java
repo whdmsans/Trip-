cd91195587e41c83a083874b54761f20acb9a89e
whdmsans <joeunmun@naver.com>
2022-12-19 00:10:46 +0900

    현재위치 표시하기 수정중(다른 위치가 뜨는 오류 있음)

--- app/src/main/java/com/example/intentexample/Home.java
+++ app/src/main/java/com/example/intentexample/Home.java
@@ -1,23 +1,100 @@
 package com.example.intentexample;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
-public class Home extends Fragment {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
 
-    public Home(){
+import java.util.Map;
 
-    }
+public class Home extends Fragment implements OnMapReadyCallback {
 
-    @Nullable
+    private NaverMap naverMap;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    public Home(){}
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home, container, false);
+
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.home,
+                container, false);
+
+        FragmentManager fm = getChildFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+
+        naverMap.setLocationSource(locationSource);
+        requestPermissions(PERMISSIONS,LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+//        if (locationSource.onRequestPermissionsResult(
+//                requestCode, permissions, grantResults)) {
+//            if (!locationSource.isActivated()) { // 권한 거부됨
+//                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+//                return;
+//            }
+//            else{
+//                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+//            }
+//        }
+//        super.onRequestPermissionsResult(
+//                requestCode, permissions, grantResults);
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // request code와 권한획득 여부 확인
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
     }
 }
