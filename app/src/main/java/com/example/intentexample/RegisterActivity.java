871595bc6f5fa630797311fe66e13b29195b3cc1
whdmsans <joeunmun@naver.com>
2022-12-20 00:48:40 +0900

    회원가입시 이름 추가로 입력받도록 수정

--- app/src/main/java/com/example/intentexample/RegisterActivity.java
+++ app/src/main/java/com/example/intentexample/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.intentexample;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +18,13 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabaseRef;
-    private EditText mEtEmail, mEtPwd;
+    private EditText mEtEmail, mEtPwd, mEtuserName;
     private Button mBtnRegister;
 
     @Override
@@ -35,6 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         mEtEmail = findViewById(R.id.et_email);
         mEtPwd = findViewById(R.id.et_pwd);
+        mEtuserName = findViewById(R.id.et_userName);
         mBtnRegister = findViewById(R.id.btn_gregister);
 
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
@@ -48,12 +52,27 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                            User user = new User();
-                            user.setIdToken(firebaseUser.getUid());
-                            user.setEmailId(firebaseUser.getEmail());
-                            user.setPassword(strPwd);
-                            
-                            mDatabaseRef.child("User").child(firebaseUser.getUid()).setValue(user);
+
+                            String email = firebaseUser.getEmail();
+                            String uid = firebaseUser.getUid();
+                            String name = mEtuserName.getText().toString().trim();
+
+                            //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                            HashMap<Object,String> hashMap = new HashMap<>();
+
+                            hashMap.put("uid",uid);
+                            hashMap.put("email",email);
+                            hashMap.put("name",name);
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = database.getReference("Users");
+                            reference.child(uid).setValue(hashMap);
+
+
+                            //가입이 이루어져을시 가입 화면을 빠져나감.
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
 
                             Toast.makeText(RegisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
                         } else {
@@ -61,6 +80,7 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     }
                 });
+
             }
         });
 
